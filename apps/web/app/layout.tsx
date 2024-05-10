2126e316da99cf8a5cabc41a8966c407cd20bd71
sjana7797 <sjana7797@gmail.com>
2024-05-10 14:16:06 +0530

    feat: adding tailwind and shadcn ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,12 @@
-import "./globals.css";
+import "@repo/ui/globals.css";
+import { cn } from "@repo/ui/utils";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Turborepo",
@@ -16,7 +20,14 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
